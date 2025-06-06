47853a3d08abd63ab4d09810b22ed30a9f599458
Steven Zhang <stevenzhangx@163.com>
2025-06-06 10:05:42 +0800

    Delete migrations  Please enter the commit message for your changes. Lines starting

--- Platform/start.py
+++ Platform/start.py
@@ -1,5 +1,6 @@
 import os
 work_dir = os.path.dirname(os.path.abspath(__file__))
+
 os.system(f"python \"{work_dir}/manage.py\" makemigrations task_manager")
 os.system(f"python \"{work_dir}/manage.py\" makemigrations user_system")
 os.system(f"python \"{work_dir}/manage.py\" makemigrations")
